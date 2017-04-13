0d216e6b78d2b695e19b6dc95eb569c6ea0d7f68
pikai <51418996@qq.com>
2017-04-13 15:22:19 +0800

    完成消息处理并返回响应文本消息

--- WeiXin-Validation/src/main/java/com/weixin/validation/app/controller/WxCoreController.java
+++ WeiXin-Validation/src/main/java/com/weixin/validation/app/controller/WxCoreController.java
@@ -2,19 +2,21 @@ package com.weixin.validation.app.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import com.weixin.validation.app.service.CoreService;
 import com.weixin.validation.util.WeiXinSignUtil;
 
 @Controller
 @RequestMapping("/core")
 public class WxCoreController {
+	
+	@Autowired
+	private CoreService coreService;
 	
 	@RequestMapping(value="/init",method=RequestMethod.GET)
 	public void core(HttpServletRequest request,HttpServletResponse response) throws IOException{
@@ -33,6 +35,22 @@ public class WxCoreController {
 			}
 	        outer.close();
 	        outer = null;
+		}
+	}
+	
+	@RequestMapping(value="/init",method=RequestMethod.POST)
+	public void processMessage(HttpServletRequest request,HttpServletResponse response) throws IOException{
+		{
+			//微信消息的接收，处理，响应
+			request.setCharacterEncoding("UTF-8");//防止中文乱码
+			response.setCharacterEncoding("UTF-8");
+			
+			String respXmlData = coreService.processRequest(request);
+			
+			//响应消息
+			PrintWriter writer = response.getWriter();
+			writer.print(respXmlData);
+			writer.close();
 		}
 	}
 }
---
+++ WeiXin-Validation/src/main/java/com/weixin/validation/util/MessageUtil.java
@@ -0,0 +1,133 @@
+package com.weixin.validation.util;
+
+import java.io.InputStream;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.core.util.QuickWriter;
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
+import com.thoughtworks.xstream.io.xml.PrettyPrintWriter;
+import com.thoughtworks.xstream.io.xml.XppDriver;
+import com.weixin.wxmessager.reponse.Article;
+import com.weixin.wxmessager.reponse.BaseResponseMessage;
+import com.weixin.wxmessager.reponse.NewsResponseMessage;
+
+/**
+ * 微信消息处理工具类
+ */
+public class MessageUtil {
+
+	// 请求消息类型：文本
+    public static final String REQ_MESSAGE_TYPE_TEXT = "text";
+    // 请求消息类型：图片
+    public static final String REQ_MESSAGE_TYPE_IMAGE = "image";
+    // 请求消息类型：语音
+    public static final String REQ_MESSAGE_TYPE_VOICE = "voice";
+    // 请求消息类型：视频
+    public static final String REQ_MESSAGE_TYPE_VIDEO = "video";
+    // 请求消息类型：小视频
+    public static final String REQ_MESSAGE_TYPE_SHORTVIDEO = "shortvideo";
+    // 请求消息类型：地理位置
+    public static final String REQ_MESSAGE_TYPE_LOCATION = "location";
+    // 请求消息类型：链接
+    public static final String REQ_MESSAGE_TYPE_LINK = "link";
+
+    // 请求消息类型：事件推送
+    public static final String REQ_MESSAGE_TYPE_EVENT = "event";
+
+    // 事件类型：subscribe(订阅)
+    public static final String EVENT_TYPE_SUBSCRIBE = "subscribe";
+    // 事件类型：unsubscribe(取消订阅)
+    public static final String EVENT_TYPE_UNSUBSCRIBE = "unsubscribe";
+    // 事件类型：scan(用户已关注时的扫描带参数二维码)
+    public static final String EVENT_TYPE_SCAN = "scan";
+    // 事件类型：LOCATION(上报地理位置)
+    public static final String EVENT_TYPE_LOCATION = "LOCATION";
+    // 事件类型：CLICK(自定义菜单)
+    public static final String EVENT_TYPE_CLICK = "CLICK";
+
+    // 响应消息类型：文本
+    public static final String RESP_MESSAGE_TYPE_TEXT = "text";
+    // 响应消息类型：图片
+    public static final String RESP_MESSAGE_TYPE_IMAGE = "image";
+    // 响应消息类型：语音
+    public static final String RESP_MESSAGE_TYPE_VOICE = "voice";
+    // 响应消息类型：视频
+    public static final String RESP_MESSAGE_TYPE_VIDEO = "video";
+    // 响应消息类型：音乐
+    public static final String RESP_MESSAGE_TYPE_MUSIC = "music";
+    // 响应消息类型：图文
+    public static final String RESP_MESSAGE_TYPE_NEWS = "news";
+    
+    /**
+     * 解析微信服务器发来的请求消息(XML)
+     */
+    public static Map<String, String> parseRequestXml(HttpServletRequest request) throws Exception{
+    	{
+    		Map<String, String> map = new HashMap<String, String>();
+    		
+    		InputStream inputStream = request.getInputStream();//请求输入流
+    		SAXReader reader = new SAXReader();
+    		Document document = reader.read(inputStream);
+    		
+    		Element root = document.getRootElement();//获取根元素，然后遍历子元素存储到map中
+    		@SuppressWarnings("unchecked")
+			List<Element> childs = root.elements();
+    		for (Element e : childs) {
+				map.put(e.getName(), e.getText());
+			}
+    		
+    		inputStream.close();
+    		inputStream = null;
+    		
+    		return map;
+    	}
+    }
+    
+    /**
+     * 将响应消息对象转换为xml数据
+     * @param t BaseResponseMessage子类实例
+     * @return
+     */
+    public static <T extends BaseResponseMessage> String respMessageToXml(T t){
+    	xstream.alias("xml", t.getClass());
+    	//图文混排的回复消息有特殊处理
+		if (t instanceof NewsResponseMessage) {
+			xstream.alias("item", new Article().getClass());
+		}
+    	return xstream.toXML(t);
+    }    
+    
+    
+    /*
+     * 扩展xstream使其支持CDATA
+     */
+    private static XStream xstream = new XStream(new XppDriver() {
+        public HierarchicalStreamWriter createWriter(Writer out) {
+            return new PrettyPrintWriter(out) {
+                // 对所有xml节点的转换都增加CDATA标记
+                boolean cdata = true;
+
+                public void startNode(String name, @SuppressWarnings("rawtypes") Class clazz) {
+                    super.startNode(name, clazz);
+                }
+
+                protected void writeText(QuickWriter writer, String text) {
+                    if (cdata) {
+                        writer.write("<![CDATA[");
+                        writer.write(text);
+                        writer.write("]]>");
+                    } else {
+                        writer.write(text);
+                    }
+                }
+            };
+        }
+    });
+}
