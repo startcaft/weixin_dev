dc7c9e816fc4913e3fae9d358375920db3db658e
pikai <51418996@qq.com>
2017-04-14 10:24:44 +0800

    WeiXinConstant类常量CREATE_MENU_URL，创建自定义菜单接口URL地址

--- WeiXin-Validation/src/main/java/com/weixin/validation/util/WeiXinConstant.java
+++ WeiXin-Validation/src/main/java/com/weixin/validation/util/WeiXinConstant.java
@@ -9,11 +9,14 @@ public class WeiXinConstant {
 	public final static String APPID = "wx898bf299e87c9656";
 	
 	/**应用密钥**/
-	public final static String APPSECRET = "359f181504949525bdb4775d18fb4d1d";
+	public final static String APPSECRET = "b766b8f0e4adbc39d8d9d5d17ea7d7c3";
 	
 	/**请求access_token(微信全局唯一的接口调用凭据)的url地址，使用的时候将字【符串中的APPID】替换成本类【常量APPID】，将【字符串APPSECRET】替换成本类【常量APPSECRET】**/
 	public final static String ACCESS_TOKEN_URL = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET";
 	
-	/**获知微信服务器的IP地址列表的url地址，使用的时候将【字符串ACCESS_TOKEN】替换成【AccessToken类的access_token属性】**/
+	/**获知微信服务器的IP地址列表的url地址，将【字符串APPSECRET】替换成本类【常量APPSECRET】**/
 	public final static String SERVER_IP_URL = "https://api.weixin.qq.com/cgi-bin/getcallbackip?access_token=ACCESS_TOKEN";
+	
+	/**创建自定义菜单的url地址，将【字符串APPSECRET】替换成本类【常量APPSECRET】**/
+	public final static String CREATE_MENU_URL = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=ACCESS_TOKEN";
 }
